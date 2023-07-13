0e2dbb20d989847ab6cbaebb411bf5530c58d668
akram <akramhany65@gmail.com>
2023-07-14 02:12:28 +0300

    finish implementing the logic and add some comments

--- index.js
+++ index.js
@@ -41,6 +41,31 @@ function isOperator(operator) {
     }
 }
 
+function calculateExpersion() {
+    if ((isOperator(operation[index]) || operation.length == 0)) {
+        return;
+    }
+
+    let i = 0;
+
+    while (operation.length > 1) {
+        for (let j = 0; j < operation.length; j++) {
+            if (operation[j] == '*' || operation[j] == '÷') {
+                i = j - 1;
+                break;
+            }
+        }
+        
+        let answer = operate(+operation[i], operation[i+1], +operation[i+2]);
+        
+        operation.splice(i, 3, answer);
+        i = 0;
+    }
+
+    lowerDisplay.textContent = operation[0];
+    index = 0;
+}
+
 let operation = [];
 let index = 0;
 
@@ -52,14 +77,27 @@ const buttons = document.querySelectorAll('.cell');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         const buttonContent = button.textContent;
-        if (isOperator(buttonContent)) {
-            if (!(isOperator(operation[index]) || operation.length == 0)) {    //to make sure that the last entry was not an operator or there was no thing at all
-                operation[++index] = ' ' + buttonContent + ' ';
+
+        if (isOperator(buttonContent)) {    //check if the button pressed was an operator
+            if (!(operation.length == 0 || isOperator(operation[index - 1]))) {    //to make sure that the last entry was not an operator or there was no thing at all
+                operation[++index] = buttonContent;
                 index++;
                 upperDisplay.textContent += ' ' + buttonContent + ' '; 
             }
-        } else {
-            if (operation.length == 0)
+        } else if (buttonContent == 'Clear') {  //check if the clear button was pressed
+
+            operation = [];                 //empty the operation array
+            upperDisplay.textContent = '';  //make the upperDisplay empty
+            lowerDisplay.textContent = '';  //make the lowerDisplay empty
+
+        } else if (buttonContent == '=') {  //check if the equal button was pressed
+
+            if (!(index == operation.length))
+                calculateExpersion();
+        
+        } else {    //a number was pressed
+
+            if (operation[index] === undefined)     
                 operation[index] = buttonContent;
             else
                 operation[index] += buttonContent;
