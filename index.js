d4d6a0fbcdaed8642cb25fc2356f85b4aa2d2f40
akram <akramhany65@gmail.com>
2023-07-13 16:18:34 +0300

    finish making the logic for displaying numbers when clicked

--- index.js
+++ index.js
@@ -27,11 +27,44 @@ function operate(num1, operator, num2) {
     }
 }
 
-const display = document.querySelector('.display');
+function isOperator(operator) {
+    if (operator == '+') {
+        return true;
+    } else if (operator == '-') {
+        return true;
+    } else if (operator == '*') {
+        return true;
+    } else if (operator == '÷') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let operation = [];
+let index = 0;
+
+
+const upperDisplay = document.querySelector('.upperPart');
+const lowerDisplay = document.querySelector('.lowerPart');
 const buttons = document.querySelectorAll('.cell');
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        display.textContent = display.textContent + button.textContent;
+        const buttonContent = button.textContent;
+        if (isOperator(buttonContent)) {
+            if (!(isOperator(operation[index]) || operation.length == 0)) {    //to make sure that the last entry was not an operator or there was no thing at all
+                operation[++index] = ' ' + buttonContent + ' ';
+                index++;
+                upperDisplay.textContent += ' ' + buttonContent + ' '; 
+            }
+        } else {
+            if (operation.length == 0)
+                operation[index] = buttonContent;
+            else
+                operation[index] += buttonContent;
+            
+            upperDisplay.textContent += buttonContent;
+        }
     })
 })
